8f6e9fcc994bb027799af41e7af69f3b18efad7c
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-05-22 04:25:34 +0000

    [Update] 4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def show
+    @user = User.find(params[:id])
+    end
+    
+    def create 
+    end
+    
+    def index
+        @users = User.all
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about',as:'home_about'
+  resources :users, only:[:new,:create,:index,:show,:destroy]
   resources :books, only:[:new,:create,:index,:show,:destroy]
+
 end
